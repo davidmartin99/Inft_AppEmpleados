9684987264eb7966c829a055d95881c2271186f5
davidmartin99 <davidmartinpulgar9@gmail.com>
2025-02-04 10:23:59 +0100

    04/02

--- app/src/main/java/com/example/intf_appempleados/MainActivity.kt
+++ app/src/main/java/com/example/intf_appempleados/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.intf_appempleados
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
@@ -19,10 +21,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.intf_appempleados.ui.viewmodel.AppViewModel
 import com.example.intf_appempleados.navigation.SetupNavGraph
+import java.lang.reflect.Modifier
 
 
 class MainActivity : ComponentActivity() {
@@ -45,10 +51,20 @@ fun MainScreen(viewModel: AppViewModel) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Empresa XYZ") },
+                title = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground), // Coloca tu imagen en la carpeta res/drawable
+                            contentDescription = "Logo"
+
+                        )
+
+                        Text("Splanot")
+                    }
+                },
                 actions = {
                     IconButton(onClick = { /* Acción de ajustes */ }) {
-                        Icon(Icons.Default.Settings, contentDescription = "Ajustes")
+                        Icon(Icons.Default.Settings, contentDescription = "Settings")
                     }
                     IconButton(onClick = { /* Acción de login */ }) {
                         Icon(Icons.Default.Person, contentDescription = "Login")
